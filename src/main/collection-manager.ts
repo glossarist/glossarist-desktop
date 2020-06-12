61411295deef3db98bf4b53f2031eecac7795083
Anton Strogonoff <anton@strogonoff.name>
2020-06-12 19:40:11 +0900

    fix: Update item list upon removal from collection

--- src/main/collection-manager.ts
+++ src/main/collection-manager.ts
@@ -1,7 +1,8 @@
 import { default as Manager } from 'coulomb/db/isogit-yaml/main/manager';
-
-import { ConceptCollection, ConceptRef } from '../models/concepts';
 import { listen } from 'coulomb/ipc/main';
+
+import { app } from '.';
+import { ConceptCollection, ConceptRef, MultiLanguageConcept } from '../models/concepts';
 
 
 class CollectionManager
@@ -12,17 +13,25 @@ extends Manager<ConceptCollection, string, { onlyIDs?: string[] }> {
 
     listen<{ objID: string, ids: ConceptRef[] }, { success: true }>
     (`${prefix}-add-items`, async ({ objID, ids }) => {
+      const _app = await app;
+      const conceptManager = _app.managers.concepts as Manager<MultiLanguageConcept<any>, string>;
+
       const collection = await this.read(objID);
       const newIDs = new Set([ ...collection.items, ...ids ]);
       await this.update(
         objID,
         { ...collection, items: [ ...newIDs ] },
         `Add items to ${collection.label}`);
+
+      await conceptManager.reportUpdatedData(ids.map(i => `${i}`));
       return { success: true };
     });
 
     listen<{ objID: string, ids: ConceptRef[] }, { success: true }>
     (`${prefix}-remove-items`, async ({ objID, ids }) => {
+      const _app = await app;
+      const conceptManager = _app.managers.concepts as Manager<MultiLanguageConcept<any>, string>;
+
       const collection = await this.read(objID);
       var newIDs = new Set(collection.items);
       for (const id of ids) {
@@ -32,6 +41,8 @@ extends Manager<ConceptCollection, string, { onlyIDs?: string[] }> {
         objID,
         { ...collection, items: [ ...newIDs ] },
         `Remove items from ${collection.label}`);
+
+      await conceptManager.reportUpdatedData(ids.map(i => `${i}`));
       return { success: true };
     });
   }
