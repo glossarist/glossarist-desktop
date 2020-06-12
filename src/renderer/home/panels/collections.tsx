8ff2b0c3547c3209558cc0f65209b77f7d5584ec
Anton Strogonoff <anton@strogonoff.name>
2020-06-12 18:26:18 +0900

    feat(collections panel): Add new collection

--- src/renderer/home/panels/collections.tsx
+++ src/renderer/home/panels/collections.tsx
@@ -1,14 +1,73 @@
-import React, { useContext, useRef, useEffect } from 'react';
-import { Tree, ITreeNode, Tag, Button } from '@blueprintjs/core';
+import * as crypto from 'crypto';
+import React, { useContext, useRef, useEffect, useState } from 'react';
+import { Tree, ITreeNode, Tag, Button, InputGroup } from '@blueprintjs/core';
 import { ConceptCollection } from 'models/concepts';
 import { app } from 'renderer';
 import { SourceContext } from '../contexts';
 import { PanelConfig } from '../panel-config';
+import { PanelContext } from 'coulomb-panel/panel';
+import { callIPC } from 'coulomb/ipc/renderer';
+
+
+interface PanelState {
+  addingItem?: boolean
+}
 
 
 const Panel: React.FC<{}> = function () {
   const source = useContext(SourceContext);
   const collections = app.useMany<ConceptCollection, {}>('collections', {});
+
+  if (Object.keys(collections.objects).length > 0) {
+    console.debug("Updating collections", Object.values(collections.objects)[0].items.length);
+  }
+
+  // Adding new items
+
+  const panel = useContext(PanelContext);
+  const panelState: PanelState = panel.state;
+  const addingItem = panelState.addingItem || false;
+  const [newItemLabel, setNewItemLabel] = useState<string>('');
+  const [commitInProgress, setCommitInProgress] = useState(false);
+  const newItemLabelInputRef = useRef<HTMLInputElement | null>(null);
+  const alreadyExists = Object.values(collections.objects).
+        find(c => c.label === newItemLabel.trim()) !== undefined;
+
+  useEffect(() => {
+    newItemLabelInputRef.current?.focus();
+  }, [addingItem]);
+
+  function toggleAddingLink(state: boolean) {
+    panel.setState({ addingItem: state });
+  }
+
+  function handleNewLinkChange(evt: React.FormEvent<HTMLInputElement>) {
+    setNewItemLabel((evt.target as HTMLInputElement).value.trim());
+  }
+
+  async function addNewItem() {
+    const label = (newItemLabel || '').trim();
+
+    if (label !== '' && alreadyExists === false) {
+      setCommitInProgress(true);
+
+      const newCollectionID = crypto.randomBytes(3).toString('hex');
+
+      await callIPC
+        <{ commit: boolean, object: ConceptCollection }, { success: true }>
+        ('model-collections-create-one', {
+          object: { id: newCollectionID, label, items: [] },
+          commit: true,
+        });
+
+      setNewItemLabel('');
+      setCommitInProgress(false);
+      toggleAddingLink(false);
+    }
+  }
+
+
+  // Showing existing items
 
   const treeRef = useRef<Tree>(null);
 
@@ -61,17 +120,56 @@ const Panel: React.FC<{}> = function () {
   map(collectionToNode);
 
   return (
-    <Tree
-      ref={treeRef}
-      contents={treeState}
-      onNodeClick={handleNodeClick}
-    />
+    <>
+      {addingItem
+        ? <InputGroup small
+            type="text"
+            inputRef={(ref) => { newItemLabelInputRef.current = ref }}
+            readOnly={commitInProgress}
+            rightElement={
+              <>
+                <Button
+                  small minimal intent="primary"
+                  icon="tick-circle"
+                  loading={commitInProgress}
+                  disabled={newItemLabel.trim() === ''}
+                  onClick={addNewItem}
+                  title="Commit new outgoing link" />
+              </>
+            }
+            onChange={handleNewLinkChange}
+            value={newItemLabel}
+            placeholder="New collection label"
+          />
+        : null}
+      <Tree
+        ref={treeRef}
+        contents={treeState}
+        onNodeClick={handleNodeClick}
+      />
+    </>
   );
 };
 
 
-const AddCollection: React.FC<{}> = function () {
-  return <Button icon="add" title="Add collection" disabled={true} />;
+const AddCollection: React.FC<{ isCollapsed?: boolean }> = function ({ isCollapsed }) {
+  const panel = useContext(PanelContext);
+  const panelState: PanelState = panel.state;
+  const addingItem = panelState.addingItem || false;
+
+  useEffect(() => {
+    toggleAddingItem(false);
+  }, [isCollapsed]);
+
+  function toggleAddingItem(state: boolean) {
+    panel.setState({ addingItem: state });
+  }
+
+  return <Button icon="add" title="Add collection" small minimal
+    onClick={(evt: React.MouseEvent<HTMLElement>) => {
+      evt.stopPropagation();
+      toggleAddingItem(!addingItem);
+    }} />;
 };
 
 
@@ -79,4 +177,5 @@ export default {
   Contents: Panel,
   title: "Collections",
   actions: [AddCollection],
+  TitleComponentSecondary: AddCollection,
 } as PanelConfig;
