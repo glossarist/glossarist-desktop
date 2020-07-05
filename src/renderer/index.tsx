3beaa5e1b26545582d53d3efca130fd6fbb8696c
Anton Strogonoff <anton@strogonoff.name>
2020-07-05 11:37:08 +0900

    chore: Switch to async l10n props (#30 WIP)

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,6 +1,7 @@
 import { RendererConfig } from 'coulomb/config/renderer';
 import { renderApp } from 'coulomb/app/renderer';
 import { conf as appConf, availableLanguages, defaultLanguage } from '../app';
+import { callIPC } from 'coulomb/ipc/renderer';
 
 
 export const conf: RendererConfig<typeof appConf> = {
@@ -18,13 +19,18 @@ export const conf: RendererConfig<typeof appConf> = {
 
   contextProviders: [{
     cls: () => import('coulomb/localizer/renderer/context-provider'),
-    getProps: () => ({
-      available: availableLanguages,
-      selected: defaultLanguage,
-      default: defaultLanguage,
-      // NOTE: Default language is treated as authoritative language.
-      // TODO: Support more than one authoritative language.
-    }),
+    getProps: async () => {
+      // const registerMeta = callIPC
+      // ('db-default-read', { objectID: 'branding' }).value.object;
+
+      return {
+        available: availableLanguages,
+        selected: defaultLanguage,
+        default: defaultLanguage,
+        // NOTE: Default language is treated as authoritative language.
+        // TODO: Support more than one authoritative language.
+      };
+    },
   }, {
     cls: () => import('coulomb/db/renderer/single-db-status-context-provider'),
     getProps: () => ({ dbName: 'default' }),
