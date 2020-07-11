bc3299fd3c39d3ec130055d63121961b254a4023
Anton Strogonoff <anton@strogonoff.name>
2020-07-11 17:57:06 +0900

    feat: Link help panel to doc index & bug report

--- src/renderer/home/panels/help.tsx
+++ src/renderer/home/panels/help.tsx
@@ -1,23 +1,48 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useEffect, useState } from 'react';
 import { DocsContext } from '../contexts';
 import { PanelConfig } from '../panel-config';
 import sharedStyles from '../styles.scss';
 import styles from './help.scss';
+import { ButtonGroup, Button } from '@blueprintjs/core';
+import { openHelpPage } from 'renderer/help';
 
 
 const Panel: React.FC<{}> = function () {
   const ctx = useContext(DocsContext);
   const item = ctx.hoveredItem;
+  const excerpt = item?.excerpt || '';
 
-  return (
-    <div className={styles.helpContainer}>
+  function openBugReportPage() {
+    require('electron').shell.openExternal(`https://github.com/glossarist/glossarist-desktop/issues/new?assignees=strogonoff&labels=&template=suspected-malfunction.md&title=%5BREPLACE_WITH_TITLE%5D`);
+  }
+
+  let help: JSX.Element;
+  if (excerpt.trim() !== '') {
+    help = (
       <p>
         {item?.excerpt}
         &emsp;
         {item?.readMoreURL
-          ? <span className={styles.readMore}><kbd>F1</kbd> to read more</span>
+          ? <strong className={styles.readMore}>
+              Press <kbd>F1</kbd> to read more
+            </strong>
           : null}
       </p>
+    );
+  } else {
+    help = (
+      <>
+        <ButtonGroup vertical fill>
+          <Button onClick={() => openHelpPage('/docs')}>Open online documentation</Button>
+          <Button onClick={openBugReportPage}>Report unexpected behavior</Button>
+        </ButtonGroup>
+      </>
+    );
+  }
+
+  return (
+    <div className={styles.helpContainer}>
+      {help}
     </div>
   );
 };
