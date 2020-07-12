199997ccdf4edfd8cd4a2b38d02cecedce550026
Anton Strogonoff <anton@strogonoff.name>
2020-07-13 01:42:05 +0900

    chore: Type VisualDiff component better

--- global.d.ts
+++ global.d.ts
@@ -11,7 +11,12 @@ interface HTMLElement {
 
 
 declare module "react-visual-diff" {
-  const VisualDiff: React.FC<{ left: JSX.Element, right: JSX.Element }>
+  const VisualDiff: React.FC<{
+    left: JSX.Element
+    right: JSX.Element
+    renderChange?: React.FC<{ type: 'added' | 'removed' }>
+    diffProps?: string[]
+  }>
   export = VisualDiff
 }
 
