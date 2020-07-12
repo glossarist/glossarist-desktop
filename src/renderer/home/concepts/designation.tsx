e3d4ca0d2900bbaa2e3d33c41ce78bf3097e7a5a
Anton Strogonoff <anton@strogonoff.name>
2020-07-13 01:15:16 +0900

    style: Break up a long line

--- src/renderer/home/concepts/designation.tsx
+++ src/renderer/home/concepts/designation.tsx
@@ -40,7 +40,12 @@ export const FullDesignation: React.FC<{ d: Designation }> = function ({ d }) {
         ? <span dir="ltr" className={styles.usage} title="Geographical area of usage">{d.geographicalArea}</span>
         : null}
       {d.normative_status !== 'admitted' && (d.normative_status?.trim() || '') !== ''
-        ? <strong dir="ltr" className={`${styles.normativeStatus} ${styles.label} ${normativeStatusClass}`} title="Normative status">{d.normative_status}</strong>
+        ? <strong
+              dir="ltr"
+              className={`${styles.normativeStatus} ${styles.label} ${normativeStatusClass}`}
+              title="Normative status">
+            {d.normative_status}
+          </strong>
         : null}
     </span>
   </span>
