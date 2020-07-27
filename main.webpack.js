94021627748c236102b2795da0aff163c948081b
Anton Strogonoff <anton@strogonoff.name>
2020-07-28 04:50:22 +0900

    chore(dependencies): bump (hopefully fix #91)

--- main.webpack.js
+++ main.webpack.js
@@ -1,7 +1,26 @@
 const path = require('path');
+const ThreadsPlugin = require('threads-plugin');
 
-module.exports = {
-  resolve: {
-    modules: [path.resolve(__dirname, './src'), 'node_modules']
-  }
+module.exports = function (config) {
+  config.resolve.modules = [path.resolve(__dirname, './src'), 'node_modules'];
+  config.plugins.unshift(
+    new ThreadsPlugin({ target: 'electron-node-worker' })
+  );
+  config.module.rules = config.module.rules.map(r => {
+    if (r.use.map) {
+      r.use = r.use.map(u => {
+        if (u.loader === 'ts-loader') {
+          console.debug("Original Webpack config for TS:", JSON.stringify(u, undefined, 4));
+          u.options.compilerOptions = {
+            ...(u.options.compilerOptions || {}),
+            module: 'esnext',
+          }
+          console.info("Modified Webpack config for TS:", JSON.stringify(u, undefined, 4));
+        }
+        return u;
+      })
+    }
+    return r;
+  });
+  return config;
 }
