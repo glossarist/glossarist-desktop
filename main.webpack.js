08c50a6dc33218630f223294779c87f3de78f18f
Anton Strogonoff <anton@strogonoff.name>
2020-07-30 05:56:06 +0900

    chore(webpack): clean up unused configuration

--- main.webpack.js
+++ main.webpack.js
@@ -6,21 +6,5 @@ module.exports = function (config) {
   config.plugins.unshift(
     new ThreadsPlugin({ target: 'electron-node-worker' })
   );
-  config.module.rules = config.module.rules.map(r => {
-    if (r.use.map) {
-      r.use = r.use.map(u => {
-        if (u.loader === 'ts-loader') {
-          console.debug("Original Webpack config for TS:", JSON.stringify(u, undefined, 4));
-          u.options.compilerOptions = {
-            ...(u.options.compilerOptions || {}),
-            module: 'esnext',
-          }
-          console.info("Modified Webpack config for TS:", JSON.stringify(u, undefined, 4));
-        }
-        return u;
-      })
-    }
-    return r;
-  });
   return config;
 }
