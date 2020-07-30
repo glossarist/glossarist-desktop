907348d4dec127ccc7eb232d45bbe9243b42706f
Anton Strogonoff <anton@strogonoff.name>
2020-07-30 11:50:46 +0900

    chore(build): fix native Node extension loader

--- main.webpack.js
+++ main.webpack.js
@@ -3,6 +3,15 @@ const ThreadsPlugin = require('threads-plugin');
 
 module.exports = function (config) {
   config.resolve.modules = [path.resolve(__dirname, './src'), 'node_modules'];
+
+  config.module.rules.unshift({
+    test: /\.node$/,
+    loader: 'awesome-node-loader',
+    options: {
+      name: '[name].[ext]',
+    },
+  });
+
   config.plugins.unshift(
     new ThreadsPlugin({ target: 'electron-node-worker' })
   );
